38d590391d0c01c30af9e7660ddf2cc543226dc0
hudyu17 <hengjeung.yuen@gmail.com>
2021-11-03 21:02:06 -0400

    dynamic update sites

--- background.js
+++ background.js
@@ -1,28 +1,32 @@
-console.log('background running');
+// const items = [];
+// const blockedSites = JSON.stringify(items);
+
+// localStorage.setItem('items', blockedSites);
 
 var URLStorage;
 
-var blockedSites = ["espn.com", "wikipedia.org"];
+// var blockedSites = ["espn.com", "wikipedia.org"];
 
-function interceptRequest(request)
-{
-  if(request && request.url)
-  {
-    if(request.type == "main_frame") // new page/site is loading in main window
-    {
+var items = localStorage.getItem('items');
+var blockedSites = JSON.parse(items);
+
+console.log(blockedSites);
+
+function interceptRequest(request) {
+  if(request && request.url) { 
+    if (blockedSites != null) {
+      if(request.type == "main_frame") // new page/site is loading in main window
+    { 
         for (var i = 0; i < blockedSites.length; ++i) {
-            if(request.url.indexOf(blockedSites[i]) > -1) {
+            // console.log(blockedSites[i]);
+            if(request.url.indexOf(blockedSites[i].item) > -1) {
               URLStorage = request.url;
               return {redirectUrl: chrome.extension.getURL("localpage.html")};
             }
         }
-    //   if(request.url.indexOf("espn.com") > -1)
-    //   {
-    //     URLStorage = request.url;
-    //     return {redirectUrl: chrome.extension.getURL("localpage.html")};
-    //   }
-    }
+    } 
+    }     
   }
-}
+};
 
 chrome.webRequest.onBeforeRequest.addListener(interceptRequest, {urls: ["*://*/*"]}, ['blocking']);
--- popout.js
+++ popout.js
@@ -1,8 +1,34 @@
-const items = [{"item": "espn.com"}];
+var itemsStorage = localStorage.getItem('items');
 
-const itemsStr = JSON.stringify(items);
+if (itemsStorage == null) {
+    const items = [];
+    const itemsStr = JSON.stringify(items);
+    localStorage.setItem('items', itemsStr);
+};
 
-localStorage.setItem('items', itemsStr);
+
+//create-todo <- create todo button onclick open ".new-item"
+//new-item <- if button pressed it save & hide "new-item"
+
+document.querySelector('.create-todo').addEventListener('click',function(){
+    document.querySelector('.new-item').style.display='block';
+});
+  
+document.querySelector('.new-item button').addEventListener('click',function(){
+    var itemName = document.querySelector('.new-item input').value;
+
+    if(itemName != ''){    
+        var itemsStorage = localStorage.getItem('items');
+        var itemsArr = JSON.parse(itemsStorage);
+        console.log(itemsArr);
+        itemsArr.push({"item":itemName});
+        saveItems(itemsArr);
+        fetchItems();
+        document.querySelector('.new-item input').value='';
+        document.querySelector('.new-item').style.display='none';
+        chrome.runtime.reload() // this is janky; reloads the extension so background script refreshes but not ideal
+    }
+});
 
 function fetchItems(){
     const itemsList = document.querySelector('ul.items');
@@ -10,9 +36,10 @@ function fetchItems(){
     var newItemHTML = '';
 
     try{
-        var items = localStorage.getItem('items');
-        var itemsArr = JSON.parse(items);
+        var itemsStorage = localStorage.getItem('items');
+        var itemsArr = JSON.parse(itemsStorage);
         for (var i = 0; i < itemsArr.length; i++) {
+            console.log(itemsArr[i]);
             newItemHTML += `<li>${itemsArr[i].item}</li>`;
         }
 
